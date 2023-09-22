b7a64792a69e0d3de4fcf53c026c65a018cea5b1
Alyona <glmalyona@gmail.com>
2023-09-22 17:42:48 +0300

    3 update test

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,33 +1,32 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable eol-last */
 /* eslint-disable no-undef */
-import puppetteer from 'puppeteer';
-
-jest.setTimeout(30000); // default puppeteer timeout
+import puppeteer from 'puppeteer';
 
 describe('Popover', () => {
   let browser = null;
   let page = null;
-  const baseUrl = 'http://localhost:8888';
 
-  beforeAll(async () => {
-    browser = await puppetteer.launch({
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
       headless: false,
       slowMo: 250,
       devtools: true,
     });
+
     page = await browser.newPage();
   });
 
-  afterAll(async () => {
-    await browser.close().catch(err =>
-      console.log(err));
-  });
-
   test('should add .active class for popover', async () => {
-    await page.goto(baseUrl);
+    jest.setTimeout(30000);
+    await page.goto.active('http://localhost:8888');
     const button = await page.$('[data-toggle="popover"]');
     button.click();
     await page.waitForSelector('[data-widget="popover-top"].active');
+  });
+
+  afterEach(async () => {
+    await browser.close();
   });
 });
